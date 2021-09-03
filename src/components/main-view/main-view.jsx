69d804f12df568e309c8dd95331937211c9630e6
Johannes van Woerkom <johannesvw3@gmail.com>
2021-09-03 01:03:25 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -47,7 +47,7 @@ export default class MainView extends React.Component {
       render() {
         const { movies, selectedMovie } = this.state;
     
-        //if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
     
         if (movies.length === 0) return <div className="main-view" />;
     
