da31bf4dcb966f63906f2441e3ba7ea7419ec906
Johannes van Woerkom <johannesvw3@gmail.com>
2021-09-21 13:47:46 -0400

    Added Bostrap styles to main-view, login-view, and movie-cards

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return (
+      <Card>
+        <Card.Img className="card-img" variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Button onClick={() => onMovieClick(movie)} variant="outline-primary">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
+    movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired
